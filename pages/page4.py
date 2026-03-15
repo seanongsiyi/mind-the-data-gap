74e60d2a3aa65778d07e108800d26fb23b5fd116
CarrotTheParrot <christopher.goh9@gmail.com>
2026-03-15 15:08:51 +0800

    test

--- pages/page4.py
+++ pages/page4.py
@@ -1,9 +1,9 @@
 import dash
 from dash import html
 
-dash.register_page(__name__,path="/page-2", name = "Analysis Page II")
+dash.register_page(__name__,path="/page-4", name = "Analysis Page IV")
 
 layout = html.Div([
-    html.H1("This is Page 2"),
+    html.H1("This is Page 4"),
     html.P("Welcome to the side page content.")
 ])
